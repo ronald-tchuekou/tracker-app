a4e33f54ffbf1cac7732804a080678cd4e497219
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2022-02-20 03:06:44 +0100

    First commit

---
+++ App.js
@@ -0,0 +1,38 @@
+import {createAppContainer, createSwitchNavigator} from "react-navigation";
+import {createStackNavigator} from "react-navigation-stack";
+import SignInScreen from "./src/screens/SignInScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
+import {createBottomTabNavigator} from "react-navigation-tabs";
+import TrackListScreen from "./src/screens/TrackListScreen";
+import TrackDetailScreen from "./src/screens/TrackDetailScreen";
+import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+import AccountScreen from "./src/screens/AccountScreen";
+import {Provider as AuthProvider} from "./src/contexts/AuthContext";
+import {setNavigator} from "./src/navigator-ref";
+import LoadingScreen from "./src/screens/LoadingScreen";
+
+const switchNavigator = createSwitchNavigator({
+   Loading: LoadingScreen,
+   loginFlow: createStackNavigator({
+      SignIn: SignInScreen,
+      SignUp: SignUpScreen
+   }),
+   mainFlow: createBottomTabNavigator({
+      trackListFlow: createStackNavigator({
+         TrackList: TrackListScreen,
+         TrackDetail: TrackDetailScreen
+      }),
+      TrackCreate: TrackCreateScreen,
+      Account: AccountScreen
+   })
+})
+
+const App = createAppContainer(switchNavigator)
+
+export default () => {
+   return (
+      <AuthProvider>
+         <App ref={(navigator) => setNavigator(navigator)}/>
+      </AuthProvider>
+   )
+}
